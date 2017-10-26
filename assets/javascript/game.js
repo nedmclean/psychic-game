a35c29eddba55db959abcb1227dfd3b3b8031b85
Edward McLean <SwagwardDaniels@Edwards-MacBook-Pro-2.local>
2017-10-26 10:09:00 -0700

    Update 1

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+
+
+var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+
+document.onkeyup = function(event) {
+	
+	var userGuess = event.key;
+
+	console.log(userGuess)
+
+
+   
