e8a20bf07c511271c499df20f64cdd13201a1fae
Mona Lisa <SwagwardDaniels@Edwards-MacBook-Pro-2.local>
2017-10-26 22:41:18 -0700

    bootstrap update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,31 +6,38 @@ var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 9;
+var totalGuesses = 9;
+var guessesSoFar = [];
 
 
 
 var computerGuess = computerGuess[Math.floor(Math.random() * computerGuess.length)];
 
 document.onkeyup = function(event) {
+
+
+var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
 	
 	var userGuess = event.key;
 	console.log(userGuess);
-	
+
 
 
 
 if(computerGuess===userGuess && guessesLeft >= 0){
-	alert("Nailed it!");
-	wins = wins + 1; 
 	
+	wins = wins + 1; 
+	alert("You are Psychic!");
+	var guessesSoFar = [];
 
-	console.log(guessesLeft);
+
 }
 else if ( computerGuess!==userGuess && guessesLeft>=0) {
-	alert("Noppers!");
 	guessesLeft = guessesLeft -1 ;
 	losses = losses +1;
+	var guessesSoFar = [];
 
 
 	}
@@ -45,7 +52,9 @@ else if ( computerGuess!==userGuess && guessesLeft>=0) {
       
 document.querySelector("#game").innerHTML = html;
 
+
 }
+
 
 
          
